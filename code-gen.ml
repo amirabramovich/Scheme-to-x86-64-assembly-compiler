27b4edda627ae2a2a8de8f24e282650358b1acea
user <amirabramovich@gmail.com>
2018-12-21 09:16:16 +0200

    started implementing const_table

--- code-gen.ml
+++ code-gen.ml
@@ -7,8 +7,31 @@ module type CODE_GEN = sig
 end;;
 
 module Code_Gen : CODE_GEN = struct
-  let make_consts_tbl asts = raise X_not_yet_implemented;;
+  (*  expr' list -> (constant * ('a * string)) list *)
+  let make_consts_tbl asts = 
+    (* 1. Scan the AST (one recursive pass) & collect the sexprs in all
+    Const records - The result is a list of sexprs *)
+    let rec scan_ast asts consts = 
+      match asts with
+      | car::cdr -> (match car with
+                    | Const' expr -> scan_ast cdr [car]@consts
+                    | _ -> scan_ast cdr consts)
+      | _ -> [](*or consts*) in
+    let scan_ast asts = scan_ast asts [] in
+    (* 2. Convert the list to a set (removing duplicates) *)
+    let rec remove_dups lst = 
+      match lst with
+      | [] -> []
+      | car::cdr -> car::(remove_dups (List.filter (fun e -> e <> car) cdr)) in
+    (* 3. Expand the list to include all sub-constants
+    The list should be sorted topologically *)
+    (* 4. Convert the resulting list into a set (remove all duplicates, again) *)
+    (* 5,  Go over the list, from first to last, and create the
+    constants-table *)
+    raise X_not_yet_implemented;;
+  (* expr' list -> (string * 'a) list *)
   let make_fvars_tbl asts = raise X_not_yet_implemented;;
+  (* (constant * ('a * string)) list -> (string * 'a) list -> expr' -> string *)
   let generate consts fvars e = raise X_not_yet_implemented;;
 end;;
 
