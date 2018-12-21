086c2b7fe4d7883bad0f4d86238a09ec9f0b1432
user <amirabramovich@gmail.com>
2018-12-21 10:54:23 +0200

    started implementing code-gen

--- code-gen.ml
+++ code-gen.ml
@@ -14,7 +14,7 @@ module Code_Gen : CODE_GEN = struct
     let rec scan_ast asts consts = 
       match asts with
       | car::cdr -> (match car with
-                    | Const' expr -> scan_ast cdr [car]@consts
+                    | Const' expr -> scan_ast cdr [expr]@consts
                     | _ -> scan_ast cdr consts)
       | _ -> [](*or consts*) in
     let scan_ast asts = scan_ast asts [] in
@@ -25,6 +25,10 @@ module Code_Gen : CODE_GEN = struct
       | car::cdr -> car::(remove_dups (List.filter (fun e -> e <> car) cdr)) in
     (* 3. Expand the list to include all sub-constants
     The list should be sorted topologically *)
+    let rec expand_lst lst newLst =
+      match lst with
+      | car :: cdr -> raise X_not_yet_implemented
+      | _ -> [] in
     (* 4. Convert the resulting list into a set (remove all duplicates, again) *)
     (* 5,  Go over the list, from first to last, and create the
     constants-table *)
