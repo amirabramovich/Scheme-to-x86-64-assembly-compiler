400bb097dd88401f961f69029050b709b78b5e9d
Nitsan <nitsan.soffair@gmail.com>
2019-01-10 17:21:21 +0200

    hex_char little fix

--- compiler.s
+++ compiler.s
@@ -539,7 +539,7 @@ section .data
 .fs_simple_char:
 	db "%c", 0
 .fs_hex_char:
-	db "\x%02x;", 0	
+	db "\x%02x;", 0
 .fs_tab:
 	db "\t", 0
 .fs_page:
@@ -754,8 +754,8 @@ section .data
 .fs_simple_char:
 	db "%c", 0
 .fs_hex_char:
-	db "\x%02x ; ", 0
-
+	db "\x%02x;", 0	
+	
 write_sob_closure:
 	push rbp
 	mov rbp, rsp
